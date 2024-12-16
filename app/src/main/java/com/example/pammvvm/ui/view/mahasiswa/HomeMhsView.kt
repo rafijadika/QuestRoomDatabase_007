9860e82e3313ecc5b2510eb6059cf4f2732022e4
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-12-16 22:05:36 +0700

    membuat composable fun body home mhs view

--- app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,19 +1,29 @@
 package com.example.pammvvm.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pammvvm.ui.viewmodel.HomeMhsViewModel
+import com.example.pammvvm.ui.viewmodel.HomeUiState
 import com.example.pammvvm.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeMhsView(
@@ -52,3 +62,32 @@ fun HomeMhsView(
         )
     }
 }
+
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } //Snackbar stste
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box (modifier= modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center)
+            {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            //menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let{message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)  //menampilkan snackbar
+                    }
+                }
+            }
+        }
