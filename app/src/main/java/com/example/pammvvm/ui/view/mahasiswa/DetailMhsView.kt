83ab758ac5a76644b45acb66e9988e8df8a16f39
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-12-16 21:58:25 +0700

    membuat composable delete confirmation dialog

--- app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/DetailMhsView.kt
@@ -6,13 +6,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
@@ -25,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pammvvm.data.entity.Mahasiswa
+import com.example.pammvvm.ui.costumwidget.TopAppBar
 import com.example.pammvvm.ui.viewmodel.DetailMhsViewModel
 import com.example.pammvvm.ui.viewmodel.DetailUiState
 import com.example.pammvvm.ui.viewmodel.PenyediaViewModel
@@ -183,6 +190,33 @@ fun ComponentDetailMhs(
         )
     }
 }
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    AlertDialog(onDismissRequest = {},
+        title = { Text("Delete Data")},
+        text = { Text("Apakah anda yakin ingin menghapus data?")},
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
+
+
+
+
 
 
 
