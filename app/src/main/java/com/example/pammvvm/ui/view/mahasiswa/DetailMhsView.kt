43ab4adbe9cbe79d212f6acc7cd7a7fc797677a8
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-12-16 21:49:04 +0700

    membuat composable membuat floating action button

--- app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,8 +1,13 @@
 package com.example.pammvvm.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pammvvm.ui.viewmodel.DetailMhsViewModel
 import com.example.pammvvm.ui.viewmodel.PenyediaViewModel
@@ -21,3 +26,22 @@ fun DetailMhsView(
             showBackButton = true,
             onBack = onBack
         )
+    },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {onEditClick(viewModel.detailUiState.value.detailUiEvent.nim)},
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa")
+            }
+        }){
+           )
+    }
+}
+
+
+
+
