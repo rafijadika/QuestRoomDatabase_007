1891a76138187121045a7829a2b42f7c17a50e99
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-12-16 22:04:21 +0700

    membuat composable fun home mhs view

---
+++ app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/HomeMhsView.kt
@@ -0,0 +1,54 @@
+package com.example.pammvvm.ui.view.mahasiswa
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pammvvm.ui.viewmodel.HomeMhsViewModel
+import com.example.pammvvm.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun HomeMhsView(
+    viewModel: HomeMhsViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMhs: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier )
+{
+    Scaffold (topBar = {
+        TopAppBar(
+            judul = "Daftar Mahasiswa",
+            showBackButton = false,
+            onBack = { },
+        )
+    },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddMhs,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Mahasiswa")
+            }
+        }){
+            innerPadding ->
+        val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeMhsView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
