6e3755a6c2674b7d7b073bedde6db6681445e300
Rafi Jadika <Rafi Jadika@LAPTOP-M1GUN5HA>
2024-12-16 21:55:32 +0700

    membuat composable item detail mhs

--- app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pammvvm/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,10 +1,13 @@
 package com.example.pammvvm.ui.view.mahasiswa
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
@@ -18,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pammvvm.data.entity.Mahasiswa
 import com.example.pammvvm.ui.viewmodel.DetailMhsViewModel
 import com.example.pammvvm.ui.viewmodel.DetailUiState
 import com.example.pammvvm.ui.viewmodel.PenyediaViewModel
@@ -120,6 +124,36 @@ fun BodyDetailMhs(
                     modifier = Modifier.padding(16.dp)
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun ItemDetailMhs(
+    modifier: Modifier = Modifier,
+    mahasiswa: Mahasiswa
+){
+    Card(
+        modifier = modifier.fillMaxWidth().padding(top = 20.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jeniskelamin)
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+            Spacer(modifier =Modifier.padding(5.dp))
+            ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+
         }
     }
 }
